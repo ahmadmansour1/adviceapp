1e2595a56249f0bd124fcd6f973f8a11bee22440
ahmadmansour1 <mansoour122@gmail.com>
2023-07-30 11:24:58 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '2_Application/core/services/Services_Theme.dart';
+import 'Theme.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeService(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeService>(builder: (context, themeService, child) {
+      return MaterialApp(
+        themeMode: themeService.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        home: const Placeholder(),
+      );
+    });
+  }
+}
