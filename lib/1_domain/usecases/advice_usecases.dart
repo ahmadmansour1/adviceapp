29425024c79a71b6feffc04c283ba4b03c34662e
ahmadmansour1 <mansoour122@gmail.com>
2023-07-31 11:27:31 +0300

    change in repo of domain layer

--- lib/1_domain/usecases/advice_usecases.dart
+++ lib/1_domain/usecases/advice_usecases.dart
@@ -1,14 +1,14 @@
+import 'package:adviceapp/0_data/repositories/advice_repo_impl.dart';
 import 'package:adviceapp/1_domain/entity/Entity.dart';
 import 'package:adviceapp/1_domain/failure/failure.dart';
+import 'package:adviceapp/1_domain/repository/advice_repo.dart';
 import 'package:dartz/dartz.dart';
 //Either comme from package dartz its provide a functional programming
 // function can return 2 types
 class AdviceUsecases {
+  final adviceRepo = RepooImpl();
   Future<Either<Failure ,Entity>> getAdvices() async{
-    await Future.delayed(const Duration(seconds: 3), () {});
-    // return   right(const Entity(advice: 'advice to test', id: 1));
-
-    return left( ServerFailure());
+  return adviceRepo.getAdviceFromDataSource();
 
   }
 
